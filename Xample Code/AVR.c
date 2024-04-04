c0019abfa4a0d59f055a146ef808031face0655b
flyandancexo <66555404+flyandancexo@users.noreply.github.com>
2024-04-03 18:44:21 -0700

    Create AVR.c

---
+++ Xample Code/AVR.c
@@ -0,0 +1,24 @@
+/*#######################################################
+# Name:
+# Hardware:
+# [Atmega8] - An Advanced MCU Project -
+########################################################*/
+
+#define F_CPU 8000000UL  // 8 MHz
+#include <util/delay.h>
+
+#include <inttypes.h>
+#include <avr/io.h>
+
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
+
+
+int main(void){
+
+  while(1){
+
+  }//--while
+
+}//--main 
